5aa800d7ff1518f1d02e775994780a83055cb2c6
dumb1cat <yzw828404@gmail.com>
2024-06-08 18:18:13 +0800

    Signal finish!!!QAQ

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #define MaxSize 256
 //let me study git!
 // https://www.cnblogs.com/LiuYanYGZ/p/14806139.html
+
+bool flag = false; // extern外部引用，前者必须是全局变量！！！
+volatile sig_atomic_t sigint_received = 0;
+
 void show_history() {
   int i = 0;
   HIST_ENTRY **his;
@@ -15,16 +19,16 @@ void show_history() {
 
 void handle_signal(int sig) {
     if (sig == SIGINT) {
-        printf("\nReceived SIGINT (Ctrl+C). Type 'exit' to quit the shell.\n");
+        sigint_received = 1;
     } else if (sig == SIGTSTP) {
-        printf("\nReceived SIGTSTP (Ctrl+Z). Type 'exit' to quit the shell.\n");
+        const char *message = "\nReceived SIGTSTP (Ctrl+Z). Type 'exit' to quit the shell.\n";
+        write(STDOUT_FILENO, message, strlen(message));
     }
 }
 
-
 // history 没有历史命令处理
 //& !!
-bool flag = false; // extern外部引用，前者必须是全局变量！！！
+
 int main() {
   pid_t output_command;
   char *input;
@@ -51,16 +55,25 @@ int main() {
   //   }
   // }
     while (1) {
+      //
+        if (sigint_received) {
+            const char *message = "\nReceived SIGINT (Ctrl+C). Type 'exit' to quit the shell.\n";
+            write(STDOUT_FILENO, message, strlen(message));
+            sigint_received = 0;
+        }
+      //
       input = readline(prompt);
-
+      
       // 检查输入是否为空
       if (input == NULL) {
         printf("\n");
         break;
       }
-
+      else if (*input == '\0') {
+            continue;
+      }
       // 如果输入非空，将其添加到历史记录中
-      if (*input && !(strstr(input, "!"))) {
+      else if (*input && !(strstr(input, "!"))) {
         add_history(input);
       }
 
@@ -75,6 +88,9 @@ int main() {
             perror("history error: no orders");
           }
         }
+        else if (strcmp(command, "exit") == 0) {
+          return 0;
+        }
         // 如果是外置命令，直接用exec函数族执行
         else {
           prase_command(command, argv, &argc);
@@ -85,6 +101,7 @@ int main() {
       }
     }
     // 释放内存，防止泄漏
+    free(input);
     free(command);
     for (int i = 0; i < argc; i++) {
       free(argv[i]);
